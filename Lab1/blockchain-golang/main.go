7e33656ccb165fa3cd658120f10e47d8cdb51923
jotaro <nqtien0507@gmail.com>
2023-12-02 17:56:47 +0700

    almost done

--- Lab1/blockchain-golang/main.go
+++ Lab1/blockchain-golang/main.go
@@ -1,38 +1,97 @@
 package main
 
 import (
-	"golang-blockchain/cli"
+	"golang-blockchain/blockchain"
+	"fmt"
+	"bufio"
+	"os"
+	"encoding/hex"
+
 )
 
 func main() {
-	// bc := blockchain.NewBlockchain()
+	var bc *blockchain.Blockchain
+	bc = &blockchain.Blockchain{}
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Println("##############################################")
+		fmt.Println("1. Create Blockchain \n2. Add Block \n3. Print Blockchain \n4. Update Transaction \n5. Print Merkle Tree \n6. Validate Block \n7. Exit")
+		
+		var choice int
+		fmt.Scan(&choice)
+		switch choice {
+		case 1:
+			bc = blockchain.NewBlockchain()
+			blockchain.PrintBlock(bc.Blocks[0])
+		case 2:
+			if len(bc.Blocks) == 0 {
+				fmt.Println("Create Blockchain first")
+				break
+			}
+			fmt.Println("New Transaction, One per line, Enter to stop")
+			transactions := []*blockchain.Transaction{}
+			for {
+				scanner.Scan()
+				input := scanner.Text()
+				if len(input) > 0 {
+					transactions = append(transactions, &blockchain.Transaction{Data: []byte(input)})
+				}else {
+					break
+				}
+			}
+			bc.AddBlock(transactions)
+		case 3:
+			if len(bc.Blocks) == 0 {
+				fmt.Println("Create Blockchain first")
+				break
+			}
+			for _, block := range bc.Blocks {
+				blockchain.PrintBlock(block)
+			}
+		case 4:
+			if len(bc.Blocks) == 0 {
+				fmt.Println("Create Blockchain first")
+				break
+			}
+			fmt.Println("Enter Block Number")
+			var blockNumber int
+			fmt.Scan(&blockNumber)
+			fmt.Println("Enter Transaction Number")
+			var transactionNumber int
+			fmt.Scan(&transactionNumber)
+			fmt.Println("Enter New Transaction Data")
+			scanner.Scan()
+			input := scanner.Text()
+			blockchain.UpdateTransactionData(bc, blockNumber, transactionNumber, input)
+		case 5:
+			if len(bc.Blocks) == 0 {
+				fmt.Println("Create Blockchain first")
+				break
+			}
+			fmt.Println("Enter Block Number")
+			var blockNumber int
+			fmt.Scan(&blockNumber)
+			blockchain.PrintMerkleTree(bc.Blocks[blockNumber])
+		case 6:
+			if len(bc.Blocks) == 0 {
+				fmt.Println("Create Blockchain first")
+				break
+			}
+			fmt.Println("Enter Block Number")
+			var blockNumber int
+			fmt.Scan(&blockNumber)
+			block := bc.Blocks[blockNumber]
+			merkleroot := hex.EncodeToString(block)
+			fmt.Println(merkleroot)
+			// if block.CalculateMerkleRoot() != bc.Blocks[blockNumber].MerkleRoot {
+			// 	fmt.Println("Invalid Block")
+			// }else {
+			// 	fmt.Println("Valid Block")
+			// }			
+		}
+		
 
-	// // Tạo các giao dịch
-	// transactions1 := []*blockchain.Transaction{&blockchain.Transaction{Data: []byte("Send 1 BTC to Vo Le Hoai")}, &blockchain.Transaction{Data: []byte("Send 10000 BTC to Nguyen Vu Khoi")},&blockchain.Transaction{Data: []byte("Send 1 ETH to Nguyen Vu Khoi")}}
-	// transactions2 := []*blockchain.Transaction{&blockchain.Transaction{Data: []byte("Send 2 more BTC to Pham Dang Quan")}}
-
-	// // Thêm các khối vào chuỗi khối
-	// bc.AddBlock(transactions1)
-	// bc.AddBlock(transactions2)
-
-	// for _, block := range bc.Blocks {
-    //     fmt.Printf("Block Number: %d\n", block.BlockNumber)
-	// 	fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
-	// 	fmt.Println("Transactions:")
-	// 	for _, tx := range block.Transactions {
-	// 		fmt.Printf("  - %s\n", string(tx.Data))
-	// 	}
-	// 	fmt.Printf("Hash: %x\n", block.Hash)
-	// 	fmt.Printf("Timestamp: %d\n", block.Timestamp)
-	// 	fmt.Printf("MerkleRoot: %x\n", block.MerkleRoot)
-	// 	fmt.Println()
-	// }
-
-    // fmt.Printf("Total Block: %d\n", len(bc.Blocks))
-    // blockchain.PrintMerkleTree(bc.Blocks[1])
-	// fmt.Println("Update transaction 0 block 1")
-	// blockchain.UpdateTransactionData(bc, 1, 0, "Send 2 BTC to Pham Dang Quan")
-	// blockchain.PrintMerkleTree(bc.Blocks[1])
-	cli.Run()
+	}
 
 }
+
